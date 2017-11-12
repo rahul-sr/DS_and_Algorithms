799218f6a2d2cb88b409a9ee211b90a506dc95a1
Rahul Srivastava <rahuls@zeta.tech>
2017-11-12 18:45:06 +0530

    comments added

--- tree/level-order-traversal.js
+++ tree/level-order-traversal.js
@@ -4,25 +4,32 @@
  *      10
  *     /  \
  *    1	  12
- *   /   / \    
+ *	 / \    \
  *  0   5   13
  *
  * => 10, 1, 12, 0, 5, 13
  *
  */
 
-function inOrder(tree) {
-	// traverse left
-	if(tree.left){
-		inOrder(tree.left);
-	}
+let arr = [];
+
+function levelOrder(tree) {
+	let head = null;
 
 	// print root
 	console.log(tree.data);
 
-	// trverse right
+	if(tree.left){
+		arr.push(tree.left);
+	}
+
 	if(tree.right){
-		inOrder(tree.right);
+		arr.push(tree.right);
+	}
+
+	if(arr.length){
+		head = arr.shift();
+		levelOrder(head);
 	}
 }
 
